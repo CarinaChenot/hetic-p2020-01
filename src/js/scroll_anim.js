0a4cd43313d956840281b473f852782456ab2c92
Carina Chenot <mllecarinach@gmail.com>
2017-10-26 13:47:55 +0200

    :sparkles: Home (#10)
    * :construction: WIP home
    
    * Add assets
    
    * app section
    
    * edit font
    
    * Add anims

---
+++ src/js/scroll_anim.js
@@ -0,0 +1,17 @@
+// import './vendors/io-polyfill.js' //TODO: Add sourcemaps (fix eslint ?)
+
+IntersectionObserver.prototype.POLL_INTERVAL = 100;
+
+function onIntersect(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) entry.target.classList.add('scrolled')
+  })
+}
+
+const options = {
+  rootMargin: '-200px' // To fire the event little bit after screen enters
+}
+
+const observer = new window.IntersectionObserver(onIntersect, options)
+
+;[...document.querySelectorAll('.to-animate')].forEach(element => observer.observe(element))
