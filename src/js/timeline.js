576b2a8965d368d4ee5b9de545107acb7b152bb1
alexbeno <alexbenoliel@gmail.com>
2017-11-24 00:12:47 +0100

    📦 add lethargy package
    add lethargy package and init IN timeline.js

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -1,8 +1,11 @@
+const Lethargy = require('lethargy').Lethargy
+
 class Timeline {
   constructor() {
     this.body = document.querySelector('body')
     this.url = document.location.href
     this.pageUrl = this.url.substring(this.url.lastIndexOf('/') + 1)
+    this.jamScroll = false;
   }
   addClass() {
     /**
@@ -17,13 +20,58 @@ class Timeline {
       this.body.classList.remove('body--story')
     }
   }
+  jam() {
+    let that = this;
+    this.jamScroll = true
+    setTimeout(function(){
+      that.jamScroll = false
+    }, 1200)
+  }
+  up() {
+    console.log('en haut')
+  }
+  down() {
+    console.log('en bas')
+  }
+  scrollToHome() {
+    let that = this
+    let contentHome = document.querySelector('.timeline')
+    let lethargy = new Lethargy()
+    var scroll = function scroll(e) {
+      if (that.jamScroll === false) {
+        e.preventDefault()
+        e.stopPropagation()
+        if (lethargy.check(e) === 1) {
+          that.up()
+          that.jam();
+        }
+        else if (lethargy.check(e) === -1) {
+          that.down()
+          that.jam();
+        }
+      }
+    }
+    contentHome.addEventListener('mousewheel', scroll)
+    contentHome.addEventListener('DOMMouseScroll', scroll)
+    contentHome.addEventListener('wheel', scroll)
+    contentHome.addEventListener('MozMousePixelScroll', scroll)
+  }
+  initScroll() {
+    this.scrollToHome()
+  }
   init() {
     /**
-     * init Timeline class
-     * @function addClass
+     * add body class
+     * @var this.pageUrl
+     * @var this.body
      */
 
-    this.addClass()
+    if (this.pageUrl === 'story.html') {
+      this.body.classList.add('body--story')
+      this.initScroll()
+    } else {
+      this.body.classList.remove('body--story')
+    }
   }
 }
 
