5a3935b0d232637f1d22cdc728f12a1b8bada31b
alexbeno <alexbenoliel@gmail.com>
2017-11-24 12:46:36 +0100

    ✨ scrollin section for timeline
    add scrolling section script for timeline page IN timeline.js

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -3,9 +3,15 @@ const Lethargy = require('lethargy').Lethargy
 class Timeline {
   constructor() {
     this.body = document.querySelector('body')
+    this.main = document.querySelector('.story')
+    this.content = document.querySelector('.timeline--1')
+    this.allSlide = document.querySelectorAll('.timeline').length
     this.url = document.location.href
     this.pageUrl = this.url.substring(this.url.lastIndexOf('/') + 1)
-    this.jamScroll = false;
+    this.jamScroll = false
+    this.translate = 0
+    this.index = 1
+    this.lethargy = new Lethargy()
   }
   addClass() {
     /**
@@ -21,43 +27,61 @@ class Timeline {
     }
   }
   jam() {
-    let that = this;
+    let that = this
     this.jamScroll = true
-    setTimeout(function(){
+    setTimeout(function() {
       that.jamScroll = false
+      console.log(that.jamScroll)
     }, 1200)
   }
   up() {
-    console.log('en haut')
+    this.translate += 100
+    this.main.style.transform = 'translateY(' + this.translate + 'vh)'
+    if (this.index > 0) {
+      this.index -= 1
+    }
+    this.update()
   }
   down() {
-    console.log('en bas')
+    this.translate -= 100
+    this.main.style.transform = 'translateY(' + this.translate + 'vh)'
+    if (this.index < this.allSlide - 1) {
+      this.index += 1
+    }
+    this.update()
   }
-  scrollToHome() {
+  update() {
+    let activeItem = document.querySelector('.timeline--active')
+    this.content = document.querySelector('.timeline--' + this.index)
+    activeItem.classList.remove('timeline--active')
+    this.content.classList.add('timeline--active')
+    this.scrollEvent()
+  }
+
+  lethargyScroll(e) {
     let that = this
-    let contentHome = document.querySelector('.timeline')
-    let lethargy = new Lethargy()
-    var scroll = function scroll(e) {
-      if (that.jamScroll === false) {
-        e.preventDefault()
-        e.stopPropagation()
-        if (lethargy.check(e) === 1) {
-          that.up()
-          that.jam();
-        }
-        else if (lethargy.check(e) === -1) {
-          that.down()
-          that.jam();
-        }
+    if (that.jamScroll === false) {
+      e.preventDefault()
+      e.stopPropagation()
+      if (that.lethargy.check(e) === 1) {
+        that.up()
+        that.jam()
+      } else if (that.lethargy.check(e) === -1) {
+        that.down()
+        that.jam()
       }
     }
-    contentHome.addEventListener('mousewheel', scroll)
-    contentHome.addEventListener('DOMMouseScroll', scroll)
-    contentHome.addEventListener('wheel', scroll)
-    contentHome.addEventListener('MozMousePixelScroll', scroll)
   }
+  scrollEvent() {
+    let that = this
+    this.content.addEventListener('mousewheel', function(e) { that.lethargyScroll(e) })
+    this.content.addEventListener('DOMMouseScroll', function(e) { that.lethargyScroll(e) })
+    this.content.addEventListener('wheel', function(e) { that.lethargyScroll(e) })
+    this.content.addEventListener('MozMousePixelScroll', function(e) { that.lethargyScroll(e) })
+  }
+
   initScroll() {
-    this.scrollToHome()
+    this.scrollEvent()
   }
   init() {
     /**
