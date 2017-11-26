636e9ab5dc929692977b51859434cfdea0f60288
Carina Chenot <mllecarinach@gmail.com>
2017-11-26 16:58:36 +0100

    :sparkles: Add 404 page (#30)

--- src/js/history.js
+++ src/js/history.js
@@ -12,21 +12,34 @@ navLink.forEach(link => {
   })
 })
 
-function getContent(filename, redirect = false) {
+const menu = document.querySelector('.header')
+
+history.listen((location, action) => {
+  if (menu.classList.contains('isOpen')) {
+    menu.classList.remove('isOpen')
+  }
+})
+
+getContent(history.location.pathname)
+
+function getContent(filename) {
   if (filename.charAt(0) === '/') filename = filename.substr(1)
   if (filename === '') filename = 'home'
 
-  fetch(filename + '.html')
-    .then((res) => {
-      return res.text()
-    })
-    .then((data) => {
-      if (redirect) {
-        if (filename === 'home') filename = ''
-        history.push('/' + filename, { some: 'state' })
-      }
-      main.innerHTML = data
-    })
+  fetch(filename + '.html').then(response => {
+    if (response.ok) {
+      if (filename === 'home') filename = ''
+      response.text().then(data => {
+        history.push('/' + filename)
+        main.innerHTML = data
+      })
+    } else {
+      history.push('/404')
+      fetch('404.html').then(response => {
+        response.text().then(data => {
+          main.innerHTML = data
+        })
+      })
+    }
+  })
 }
-
-getContent(history.location.pathname)
