d44be928da8ae2186c2f8f4c7f64c3c3fd367189
alexbeno <alexbenoliel@gmail.com>
2017-11-26 14:38:17 +0100

    ✨ add navigation script
    add naviagtion script for slide IN story.pug, timeline.js, nav.scss

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -4,13 +4,14 @@ class Timeline {
   constructor() {
     this.body = document.querySelector('body')
     this.main = document.querySelector('.story')
-    this.content = document.querySelector('.timeline--1')
+    this.content = document.querySelector('.timeline--active')
+    this.nav = document.querySelectorAll('.timelineNav__item')
     this.allSlide = document.querySelectorAll('.timeline').length
     this.url = document.location.href
     this.pageUrl = this.url.substring(this.url.lastIndexOf('/') + 1)
     this.jamScroll = false
     this.translate = 0
-    this.index = 1
+    this.index = 0
     this.lethargy = new Lethargy()
   }
   addClass() {
@@ -31,11 +32,11 @@ class Timeline {
     this.jamScroll = true
     setTimeout(function() {
       that.jamScroll = false
-      console.log(that.jamScroll)
     }, 1200)
   }
   up() {
     this.translate += 100
+    console.log(this.translate)
     this.main.style.transform = 'translateY(' + this.translate + 'vh)'
     if (this.index > 0) {
       this.index -= 1
@@ -44,13 +45,14 @@ class Timeline {
   }
   down() {
     this.translate -= 100
+    console.log(this.translate)
     this.main.style.transform = 'translateY(' + this.translate + 'vh)'
     if (this.index < this.allSlide - 1) {
       this.index += 1
     }
     this.update()
   }
-  nav(attribute) {
+  updateNav(attribute) {
     let activeItem = document.querySelector('.timelineNav__item--active')
     if (activeItem != null) {
       activeItem.classList.remove('timelineNav__item--active')
@@ -64,10 +66,8 @@ class Timeline {
     this.content = document.querySelector('.timeline--' + this.index)
     activeItem.classList.remove('timeline--active')
     this.content.classList.add('timeline--active')
-    if (this.content.getAttribute('data-date')) {
-      let attribute = this.content.getAttribute('data-date')
-      this.nav(attribute)
-    }
+    let attribute = this.content.getAttribute('data-date')
+    this.updateNav(attribute)
     this.scrollEvent()
   }
 
@@ -94,9 +94,27 @@ class Timeline {
     this.content.addEventListener('wheel', function(e) { that.lethargyScroll(e) })
     this.content.addEventListener('MozMousePixelScroll', function(e) { that.lethargyScroll(e) })
   }
+  updateSlide(index) {
+    this.translate = -100 * index
+    this.main.style.transform = 'translateY(' + this.translate + 'vh)'
+    this.index = index
+    this.update()
+  }
 
   initScroll() {
     this.scrollEvent()
+  }
+  initNav() {
+    let size = this.nav.length
+    let that = this
+
+    for (let i = 0; i < size; i++) {
+      that.nav[i].addEventListener('click', function(e) {
+        e.preventDefault()
+        let indexSlide = that.nav[i].getAttribute('data-slide')
+        that.updateSlide(parseInt(indexSlide))
+      })
+    }
   }
   init() {
     /**
@@ -108,6 +126,7 @@ class Timeline {
     if (this.pageUrl === 'story.html') {
       this.body.classList.add('body--story')
       this.initScroll()
+      this.initNav()
     } else {
       this.body.classList.remove('body--story')
     }
