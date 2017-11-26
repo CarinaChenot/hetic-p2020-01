3e425015529ed088930c2cba6517e16656d0bf69
alexbeno <alexbenoliel@gmail.com>
2017-11-26 14:55:23 +0100

    💡 add documentation
    add documentation IN timeline.js

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -14,20 +14,12 @@ class Timeline {
     this.index = 0
     this.lethargy = new Lethargy()
   }
-  addClass() {
+  jam() {
     /**
-     * add body class
-     * @var this.pageUrl
-     * @var this.body
+     * block momentarily the scroll
+     * @var this.jamScroll bool for block the scroll
      */
 
-    if (this.pageUrl === 'story.html') {
-      this.body.classList.add('body--story')
-    } else {
-      this.body.classList.remove('body--story')
-    }
-  }
-  jam() {
     let that = this
     this.jamScroll = true
     setTimeout(function() {
@@ -35,8 +27,15 @@ class Timeline {
     }, 1200)
   }
   up() {
+    /**
+     * update the translation of .story when the scroll go up
+     * @var this.translate value of the translate
+     * @var this.main main content of the page
+     * @var this.index index of the current slide
+     * @function udpate update active class
+     */
+
     this.translate += 100
-    console.log(this.translate)
     this.main.style.transform = 'translateY(' + this.translate + 'vh)'
     if (this.index > 0) {
       this.index -= 1
@@ -44,8 +43,15 @@ class Timeline {
     this.update()
   }
   down() {
+    /**
+     * update the translation of .story when the scroll go down
+     * @var this.translate value of the translate
+     * @var this.main main content of the page
+     * @var this.index index of the current slide
+     * @function udpate update active class
+     */
+
     this.translate -= 100
-    console.log(this.translate)
     this.main.style.transform = 'translateY(' + this.translate + 'vh)'
     if (this.index < this.allSlide - 1) {
       this.index += 1
@@ -53,6 +59,13 @@ class Timeline {
     this.update()
   }
   updateNav(attribute) {
+    /**
+     * add and remove active class for navigation
+     * @param attribute current data-slide attribute
+     * @var activeItem active slide
+     * @var navigationItem active navigation item
+     */
+
     let activeItem = document.querySelector('.timelineNav__item--active')
     if (activeItem != null) {
       activeItem.classList.remove('timelineNav__item--active')
@@ -62,6 +75,14 @@ class Timeline {
     navigationItem.classList.add('timelineNav__item--active')
   }
   update() {
+    /**
+     * update active class for the current slide and udpate the content var for scroll event
+     * @var activeItem
+     * @var this.content
+     * @function updateNav add active class for navigation
+     * @function scrollEvent scroll event of the current slide
+     */
+
     let activeItem = document.querySelector('.timeline--active')
     this.content = document.querySelector('.timeline--' + this.index)
     activeItem.classList.remove('timeline--active')
@@ -72,6 +93,14 @@ class Timeline {
   }
 
   lethargyScroll(e) {
+    /**
+     * lethargy checking for direction of the scroll
+     * @var this.lethargy define the lethragy librairi
+     * @function up translation up
+     * @function down translation down
+     * @function jam block the scroll
+     */
+
     let that = this
     if (window.innerWidth > 768) {
       if (that.jamScroll === false) {
@@ -88,6 +117,12 @@ class Timeline {
     }
   }
   scrollEvent() {
+    /**
+     * scroll event on the current content
+     * @var this.content current active slide for scroll event
+     * @function lethargyScroll lethargy checking for scroll
+     */
+
     let that = this
     this.content.addEventListener('mousewheel', function(e) { that.lethargyScroll(e) })
     this.content.addEventListener('DOMMouseScroll', function(e) { that.lethargyScroll(e) })
@@ -95,6 +130,14 @@ class Timeline {
     this.content.addEventListener('MozMousePixelScroll', function(e) { that.lethargyScroll(e) })
   }
   updateSlide(index) {
+    /**
+     * update slide translation when click on nav
+     * @var this.translate value of the translate
+     * @var this.main main content of the page
+     * @var this.index index of the current slide
+     * @function udpate update active class
+     */
+
     this.translate = -100 * index
     this.main.style.transform = 'translateY(' + this.translate + 'vh)'
     this.index = index
@@ -102,9 +145,22 @@ class Timeline {
   }
 
   initScroll() {
+    /**
+     * init the scroll event
+     * @function scrollEvent() scroll event on the current content
+     */
+
     this.scrollEvent()
   }
   initNav() {
+    /**
+     * init the click event on slide navigation
+     * @var this.nav all nav item
+     * @var size number of nav item
+     * @var indexSlide current index of nav item
+     * @function updateSlide update slide translation
+     */
+
     let size = this.nav.length
     let that = this
 
@@ -119,8 +175,8 @@ class Timeline {
   init() {
     /**
      * add body class
-     * @var this.pageUrl
-     * @var this.body
+     * @var this.pageUrl link of the current page
+     * @var this.body select the body
      */
 
     if (this.pageUrl === 'story.html') {
