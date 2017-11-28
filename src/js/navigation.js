cded7aec401c8422565c2d4915955b79d5e15225
Carina Chenot <mllecarinach@gmail.com>
2017-11-28 12:06:35 +0100

    :zap: Add cache storage for fetched html

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -6,6 +6,7 @@ import { init3dHelmet } from './3dHelmet'
 const HOME_PAGE = 'home'
 const STORY_PAGE = 'story'
 const ERR_PAGE = '404'
+const STORAGE_KEY = 'ch_page_'
 
 class Navigation {
   constructor() {
@@ -16,23 +17,23 @@ class Navigation {
     this.init()
   }
   init() {
-    this.getFile(this.currentPage)
-    this.$link.forEach(link => this.addEvents(link))
+    this.getContent(this.currentPage)
+
+    this.$link.forEach(link => {
+      link.addEventListener('click', e => {
+        e.preventDefault()
+        this.currentPage = link.getAttribute('data-link')
+        this.update()
+      })
+    })
   }
   getCurrentPage() {
     let location = this.history.location.pathname.substr(1)
 
     return location === '' ? HOME_PAGE : location
   }
-  addEvents(link) {
-    link.addEventListener('click', e => {
-      e.preventDefault()
-      this.currentPage = link.getAttribute('data-link')
-      this.update()
-    })
-  }
   update() {
-    this.getFile()
+    this.getContent()
     this.updateUrl()
   }
   updateUrl() {
@@ -51,10 +52,29 @@ class Navigation {
         break
     }
   }
+  getContent() {
+    let content = this.getStorage()
+
+    if (content) {
+      this.updateDOM(content)
+    } else {
+      this.getFile()
+    }
+  }
+  getStorage() {
+    return localStorage.getItem(STORAGE_KEY + this.currentPage)
+  }
+  setStorage(data) {
+    localStorage.setItem(STORAGE_KEY + this.currentPage, data)
+  }
   getFile(filename = this.currentPage) {
     fetch(filename + '.html').then(response => {
-      if (response.ok) response.text().then(data => this.updateDOM(data))
-      else this.handleError()
+      if (response.ok) {
+        response.text().then(data => {
+          this.updateDOM(data)
+          this.setStorage(data)
+        })
+      } else this.handleError()
     })
   }
   handleError(err = ERR_PAGE) {
